86a20bbc81f641ddcc4e04a67e542eafdcb46c7d
daniel-717 <denildaniel502@gmail.com>
2025-09-08 19:06:49 +0530

    api.js updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api', // This will be proxied to our backend server
+  baseURL: import.meta.env.VITE_API_URL || '/api', // This will be proxied to our backend server
 });
 
 // Interceptor to add the auth token to every request if it exists
